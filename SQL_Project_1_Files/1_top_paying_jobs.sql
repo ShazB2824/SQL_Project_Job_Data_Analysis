fba6f6e18b177a7eb9a59bf91359b3acb6be0335
ShawaizB2004 <shawaizbhatti1@gmail.com>
2025-12-04 22:09:28 +1100

    Updates to 1_top_paying_jobs.sql

--- SQL_Project_1_Files/1_top_paying_jobs.sql
+++ SQL_Project_1_Files/1_top_paying_jobs.sql
@@ -1,1 +1,25 @@
--- will update later
+/* 
+    Question: What are the top paying Data Analyst roles?
+    - Identify the top 10 highest paying Data Analyst roles that are available remotely
+    - Focus on job postings with specified salaries (remove NULLs)
+    - Why? Highlight the top paying opportunities for Data Analysts, offering insights into the top skills and roles
+*/
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    company_dim.name AS company_name
+FROM
+    job_postings_fact AS job_postings 
+LEFT JOIN company_dim ON job_postings.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    job_schedule_type = 'Full-time' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
